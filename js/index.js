4e542f90bb6b458d56676cac666d031a2f7b86cf
LingsRanran <1078421081@qq.com>
2018-06-13 17:55:22 +0800

    fifth commit

--- js/index.js
+++ js/index.js
@@ -1,10 +1,22 @@
+var images = {
+    'ball':'img/ball.png',
+    'block':'img/block.gif',
+    'box':'img/box.png',
+    'wall':'img/wall.png',
+    'up':'img/up.png',
+    'down':'img/down.png',
+    'left':'img/left.png',
+    'right':'img/right.png',
+};
 
+//初始化
+function init(){
 
+}
 
-
-
-
-
+function drawMap(){
+    
+}
 
 
 
@@ -16,3 +28,4 @@ function showInfo(){
 
 };
 
+preImgLoad()
